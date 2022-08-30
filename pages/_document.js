db590904e60e5b696f4247d3a8d4eca85dd0f7d9
Jabgomez <josebaquerizo_1999@hotmail.com>
2022-08-29 19:31:36 -0500

    image src has been added

--- pages/_document.js
+++ pages/_document.js
@@ -18,6 +18,7 @@ class MyDocument extends Document {
                     <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"/> 
                     <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🏡</text></svg>"></link>
                     <link rel="manifest" href="/manifest.json"/>
+                    <link rel="image_src" href="/images/profile.png" />
 
                     <meta property="og:title" content="José Baquerizo"/>
                     <meta name="twitter:title" content="José Baquerizo"/>
@@ -26,8 +27,8 @@ class MyDocument extends Document {
                     <meta property="og:description" content="An Ecuadorian Web developer and UI/UX Designer based in Salinas"/>
                     <meta name="twitter:description" content="An Ecuadorian Web developer and UI/UX Designer based in Salinas"/>
                     
-                    <meta property="og:image" content="/images/logo.png"/>
-                    <meta name="twitter:image" content="/imges/logo.png"/>
+                    <meta property="og:image" content="/images/profile.png"/>
+                    <meta name="twitter:image" content="/images/profile.png"/>
                 </Head>
                 <body className="bg-background text-gray-800 transition-all">
                     <Main />
