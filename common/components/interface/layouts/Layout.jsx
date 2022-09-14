c359d5e2685d40515737881a836e6347c6f7a4bd
Jabgomez <josebaquerizo_1999@hotmail.com>
2022-09-14 17:46:06 -0500

    Landscape background has been fixed

--- common/components/interface/layouts/Layout.jsx
+++ common/components/interface/layouts/Layout.jsx
@@ -1,16 +1,16 @@
 import Footer from "./Footer";
 import Header from "./Header";
 
-const { Fragment } = require("react")
-
 const Layout = ({ children }) => {
-    return ( 
-        <div className="min-h-screen bg-landscape bg-bottom lg:bg-contain bg-no-repeat !bg-opacity-25 flex flex-col max-w-384 mx-auto">
-            <Header/>
-            <div className="grow mt-20">
-                {children}
+    return (
+        <div className="bg-landscape bg-bottom lg:bg-contain bg-no-repeat !bg-opacity-25">
+            <div className="min-h-screen flex flex-col max-w-384 mx-auto">
+                <Header/>
+                <div className="grow mt-20">
+                    {children}
+                </div>
+                <Footer/>
             </div>
-            <Footer/>
         </div>
     );
 }
