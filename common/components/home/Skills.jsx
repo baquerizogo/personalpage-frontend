c33bcbc98ff6f278b04b7b34d3a83f073335d5ba
Jabgomez <josebaquerizo_1999@hotmail.com>
2023-02-02 16:23:47 -0500

    Update skills in hero section

--- common/components/home/Skills.jsx
+++ common/components/home/Skills.jsx
@@ -9,22 +9,25 @@ import {
 	faHtml5, 
 	faJs, 
 	faNodeJs, 
-	faReact
+	faReact,
 } from "@fortawesome/free-brands-svg-icons";
-import { faDatabase } from "@fortawesome/free-solid-svg-icons";
+
+import { faDatabase, faVial } from "@fortawesome/free-solid-svg-icons";
 
 const Skills = () => {
     return (
-        <div className="mb-8 col-span-full md:col-span-5 lg:col-start-2 lg:col-end-9 flex flex-wrap text-secondary-600">
+        <div className="mb-8 col-span-full md:col-span-5 lg:col-start-2 lg:col-end-9 flex flex-wrap">
             <IconBadge icon={faHtml5} text="HTML 5"/>
             <IconBadge icon={faCss3Alt} text="CSS 3"/>
             <IconBadge icon={faJs} text="JavaScript"/>
+            <IconBadge abbr="Ts" text="TypeScript"/>
             <IconBadge icon={faReact} text="ReactJS"/>
             <IconBadge icon={faNodeJs} text="NodeJS"/>
             <IconBadge abbr="Nx" text="NextJS"/>
             <IconBadge abbr="Ex" text="ExpressJS"/>
+            <IconBadge icon={faVial} text="Jest"/>
             <IconBadge icon={faGitAlt} text="Git/GitHub"/>
-            <IconBadge svg={Tailwind} text="Tailwind CSS" className="fill-secondary-600"/>
+            <IconBadge svg={Tailwind} text="Tailwind CSS" className="fill-slate-700"/>
             <IconBadge icon={faBootstrap} text="Bootstrap"/>
             <IconBadge icon={faDatabase} text="MongoDB"/>
             <IconBadge abbr="Ai" text="Adobe Ilustrator"/>
--- common/components/interface/badges/IconBadge.jsx
+++ common/components/interface/badges/IconBadge.jsx
@@ -2,11 +2,11 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const IconBadge = ({ icon, text, abbr, svg: Svg, className }) => {
     return (
-        <div className="inline-block bg-lowContrast/70 mr-2 my-1 px-4 py-2 rounded-full">
+        <div className="inline-block bg-slate-700 text-slate-100 mr-2 my-1 px-4 py-2 rounded-full">
             <div className="flex items-center space-x-2">
                 { icon ? <FontAwesomeIcon icon={icon} size='lg'/> : null }
                 { abbr ? <span className="text-sm font-bold">{abbr}</span> : null }
-                { Svg ? <Svg className={className || ''}/> : null}
+                { Svg ? <Svg className={`fill-slate-100 ${className || ''}`}/> : null}
                 <span className="text-sm">{text}</span>
             </div>
         </div>
